7846688740e207624e54a3b53fa1f34839aceb87
darrickpang <dkypang@yahoo.com>
2020-07-14 14:33:09 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  let addUl = document.querySelector('#tasks')
+
+  const buildLi = function(task){
+    let li = document.createElement('li')
+    li.innerHTML = task
+    addUl.appendChild(li)
+  }
+
+  const handleSubmit = function(e){
+    e.preventDefault()
+    buildLi(e.target.querySelector('#new-task-description').value)
+  }
+
+  form.addEventListener("submit", handleSubmit);
 });
