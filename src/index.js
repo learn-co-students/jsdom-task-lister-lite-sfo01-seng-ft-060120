1dddef755fa989c86890918b5fdf548b4474f9fb
candacecodes <candacehchoe@gmail.com>
2020-07-24 14:58:19 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  // your code here	  // your code here
+  let tasks = document.getElementById("tasks");
+  let form = document.querySelector("form");
+
+  // handlers
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    let description = e.target.name.value;
+    buildLi(description);
+  };
+
+  const handleDelete = function (e) {
+    e.target.parentElement.remove();
+  };
+
+  // event listeners
+  form.addEventListener("submit", handleSubmit);
+  tasks.addEventListener("click", handleDelete);
+
+  // build Li with button
+
+  const buildLi = function (description) {
+    let li = document.createElement('li')
+    li.innerText = description
+    tasks.appendChild(li)
+  };
+}); 
