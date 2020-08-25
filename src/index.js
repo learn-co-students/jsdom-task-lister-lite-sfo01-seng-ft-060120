432af6816669294da5bf6137989f5d26b2dfe95c
candacecodes <candacehchoe@gmail.com>
2020-08-25 13:34:07 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here	  // your code here
-  let tasks = document.getElementById("tasks");
-  let form = document.querySelector("form");
+  let tasks = document.getElementById('tasks') 
+let form = document.getElementById('create-task-form')
+form.addEventListener('submit', (e) => addToDoList(e))
 
-  // handlers
-  const handleSubmit = function (e) {
-    e.preventDefault();
-    let description = e.target.name.value;
-    buildLi(description);
-  };
+})
 
-  const handleDelete = function (e) {
-    e.target.parentElement.remove();
-  };
+const addToDoList = (e) => {
+  e.preventDefault()
+  let li = document.createElement('li')
+  li.innerText = e.target[0].value
+  tasks.appendChild(li)
 
-  // event listeners
-  form.addEventListener("submit", handleSubmit);
-  tasks.addEventListener("click", handleDelete);
 
-  // build Li with button
+  let deleteBtn = document.createElement('button')
+  deleteBtn.innerText = 'delete'
+  li.appendChild(deleteBtn)
+  deleteBtn.addEventListener('click', () => tasks.removeChild(li))
+  
+  // let editBtn = document.createElement('button') 
+  // editBtn.innerText = 'edit'
+  // li.appendChild(editBtn)
+  // editBtn.addEventListener('click', (e) => editTask(e))
+}
 
-  const buildLi = function (description) {
-    let li = document.createElement('li')
-    li.innerText = description
-    tasks.appendChild(li)
-  };
-}); 
+// const editTask = (e) => {
+//   console.log(e)
+// }
+
+// const deleteItem = (e) => {
+//   console.log(e) 
+//   e.target.parentElement.remove() 
+
+// }
+
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
